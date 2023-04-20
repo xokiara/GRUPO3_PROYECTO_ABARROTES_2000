d22f300cffd355045d0feaa8cef11b7c1b46177a
xokiara <kiaramoncada28@gmail.com>
2023-04-20 00:07:15 -0600

    Login
    Login

--- Proyecto/Blazor/Program.cs
+++ Proyecto/Blazor/Program.cs
@@ -1,8 +1,18 @@
+using Blazor;
+using Blazor.Interfaces;
+using Blazor.Servicios;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+Config cadena = new Config(builder.Configuration.GetConnectionString("MySQL"));
+builder.Services.AddSingleton(cadena);
+
+builder.Services.AddScoped<ILoginServicio, LoginServicio>();
+builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
 
 var app = builder.Build();
 
