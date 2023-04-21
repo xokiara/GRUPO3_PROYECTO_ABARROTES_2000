0c74e60b6dfa5f6045affce08beabfbea5d262e7
xokiara <kiaramoncada28@gmail.com>
2023-04-20 20:54:50 -0600

    Usuarios

--- Proyecto/Blazor/Pages/Usuarios/Usuarios.razor.cs
+++ Proyecto/Blazor/Pages/Usuarios/Usuarios.razor.cs
@@ -1,7 +1,19 @@
-﻿namespace Blazor.Pages.Usuarios
+﻿using Blazor.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Blazor.Pages.Usuarios
 {
     public partial class Usuarios
     {
+        [Inject] private IUsuarioServicio usuarioServicio { get; set; }
+
+        private IEnumerable<Usuario> lista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await usuarioServicio.GetListaAsync();
+        }
 
     }
 }
