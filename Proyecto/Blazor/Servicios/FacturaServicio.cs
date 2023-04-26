4b620fce0c2eddeb84d9fa4cf345baa7ac1e99da
edgardaniel77 <edgardaguilera3@gmail.com>
2023-04-25 20:00:39 -0600

    primer commit prueba

--- Proyecto/Blazor/Servicios/FacturaServicio.cs
+++ Proyecto/Blazor/Servicios/FacturaServicio.cs
@@ -1,7 +1,12 @@
-﻿namespace Blazor.Servicios
-{
-    public class FacturaServicio
-    {
+﻿using Blazor.Interfaces;
+using Datos.Interfaces;
+using Datos.Repositorios;
+using Modelos;
 
+namespace Blazor.Servicios
+{
+    public class FacturaServicio: IFacturaServicio
+    {
+       
     }
 }
