126f87347fd7bad4e78f6b431e55a654bd0a6c86
edgardaniel77 <edgardaguilera3@gmail.com>
2023-04-27 16:02:46 -0600

    commit factura

--- Proyecto/Blazor/Servicios/FacturaServicio.cs
+++ Proyecto/Blazor/Servicios/FacturaServicio.cs
@@ -3,10 +3,11 @@ using Datos.Interfaces;
 using Datos.Repositorios;
 using Modelos;
 
+
 namespace Blazor.Servicios
 {
-    public class FacturaServicio: IFacturaServicio
+    public class FacturaServicio 
     {
        
-    }
+      }
 }
