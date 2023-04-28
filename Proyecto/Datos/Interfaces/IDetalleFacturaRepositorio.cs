6e6ebd3363cdf68ec95b4def7e5f8323c68d141a
edagui77 <edgar.aguilera@unah.hn>
2023-04-28 16:45:48 -0600

    commit factura
    cambios de factura y detalle de factura

--- Proyecto/Datos/Interfaces/IDetalleFacturaRepositorio.cs
+++ Proyecto/Datos/Interfaces/IDetalleFacturaRepositorio.cs
@@ -1,7 +1,12 @@
-﻿namespace Datos.Interfaces
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Interfaces
 {
-    public class IDetalleFacturaRepositorio
+    internal interface IDetalleFacturaRepositorio
     {
-        //comentario nuevo
     }
 }
